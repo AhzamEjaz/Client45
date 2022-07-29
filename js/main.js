16fff30b7481b9900b0aa0f806aee965bbd0d0ac
0001-BSCS-19 <0001-bscs-19@web.gcu.edu.pk>
2022-07-29 22:12:43 +0500

    New changes regarding design flaws in page 1 and 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 forms_ids = document.getElementById("forms-container").children
+card_click_select = [0, 0, 0]
+
 console.log(forms_ids);
 function moveToNext(current_form_number){
     if(current_form_number == 1){
@@ -28,3 +30,30 @@ function turnOnForm(form_id){
     forms_ids[form_id].style.display = "block";
 }
 
+
+function onCardClick(card_number) {
+  console.log(card_number);
+  if(card_number==0){
+    
+    document.getElementById("replace-card").classList.add("selected-card")
+
+    document.getElementById("repair-card").classList.remove("selected-card")
+    document.getElementById("construct-card").classList.remove("selected-card")
+
+  }
+  else if(card_number==1){
+    document.getElementById("repair-card").classList.add("selected-card")
+
+    document.getElementById("replace-card").classList.remove("selected-card")
+    document.getElementById("construct-card").classList.remove("selected-card")
+
+  }
+  else if(card_number==2){
+    document.getElementById("construct-card").classList.add("selected-card")
+
+    document.getElementById("repair-card").classList.remove("selected-card")
+    document.getElementById("replace-card").classList.remove("selected-card")
+
+  }
+
+}
